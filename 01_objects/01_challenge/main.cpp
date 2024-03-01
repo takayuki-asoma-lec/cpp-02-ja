da0561269f53a8343081e846d9a3675954363ea9
takayuki.asoma <takayuki.asoma@woven-planet.global>
2024-03-01 20:23:19 +0900

    01_01

--- 01_objects/01_challenge/main.cpp
+++ 01_objects/01_challenge/main.cpp
@@ -1,1 +1,7 @@
 // コードを入力してください
+#include <iostream>
+#include "traffic_light.h"
+
+int main(){
+    std::cout << get_action(TrafficLight::green) << std::endl;
+}
--- 01_objects/01_challenge/traffic_light.cpp
+++ 01_objects/01_challenge/traffic_light.cpp
@@ -1,1 +1,15 @@
+#include "traffic_light.h"
+
 // コードを入力してください
+std::string get_action(TrafficLight light)
+{
+    // コードを入力してください
+    switch(light){
+        case TrafficLight::green:
+            return "Go";
+        case TrafficLight::yellow:
+            return "Caution";
+        case TrafficLight::red:
+            return "Stop";
+    }
+}
