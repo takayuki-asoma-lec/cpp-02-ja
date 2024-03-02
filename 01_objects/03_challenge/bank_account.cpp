da60e8979eaa14a5c42c65d624decb88a1192cd9
takayuki.asoma <takayuki.asoma@woven-planet.global>
2024-03-02 14:40:43 +0900

    01_03

--- 01_objects/03_challenge/bank_account.cpp
+++ 01_objects/03_challenge/bank_account.cpp
@@ -1,1 +1,24 @@
 // コードを入力してください
+#include "bank_account.h"
+#include <iostream>
+
+void BankAccount::deposit(const double amount){
+    if(amount < 0)
+        return;
+    balance_ += amount;
+    std::cout << balance_ << std::endl;
+}
+
+void BankAccount::withdraw(const double amount){
+    if(amount < 0)
+        return;
+    if (amount > balance_)
+    {
+        std::cout << "Insufficient balance!" << std::endl;
+        return;
+    }
+    balance_ -= amount;
+    std::cout << balance_ << std::endl;
+}
+
+double BankAccount::get_balance() { return balance_; };
--- 01_objects/03_challenge/bank_account.h
+++ 01_objects/03_challenge/bank_account.h
@@ -1,1 +1,13 @@
 // コードを入力してください
+#pragma once
+
+class BankAccount{
+public:
+    BankAccount(const double balance) : balance_(balance){};
+    void deposit(const double amount);
+    void withdraw(const double amount);
+    double get_balance();
+
+private:
+    double balance_;
+};
