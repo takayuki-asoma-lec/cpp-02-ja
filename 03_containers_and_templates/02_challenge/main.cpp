0f98a860467cdcad004def6d2d1153b4348cdd23
takayuki.asoma <takayuki.asoma@woven-planet.global>
2024-03-21 12:15:28 +0900

    03_02

--- 03_containers_and_templates/02_challenge/main.cpp
+++ 03_containers_and_templates/02_challenge/main.cpp
@@ -1,1 +1,14 @@
 // コードを入力してください
+#include "stack.hpp"
+#include <vector>
+#include <cassert>
+
+int main(){
+    Stack<int> st;
+    assert(st.is_empty());
+    st.push(1);
+    assert(!st.is_empty());
+    assert(st.top() == 1);
+    st.pop();
+    assert(st.is_empty());
+}
--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,23 @@
 // コードを入力してください
+#include <vector>
+
+template <typename T>
+class Stack{
+public:
+    void push(const T &value){
+        container_.push_back(value);
+    };
+    void pop(){
+        if(container_.size()==0)
+            return;
+        container_.pop_back();
+    };
+    T top() const{
+        return container_.back();
+    };
+    bool is_empty() const{
+        return container_.size() == 0;
+    };
+private:
+    std::vector<T> container_;
+};
