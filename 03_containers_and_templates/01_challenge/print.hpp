9781acf204d9a482792a769e356a258f8bfd0253
takayuki.asoma <takayuki.asoma@woven-planet.global>
2024-03-21 11:57:50 +0900

    03_01

--- 03_containers_and_templates/01_challenge/print.hpp
+++ 03_containers_and_templates/01_challenge/print.hpp
@@ -1,1 +1,9 @@
 // コードを入力してください
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+void print_elements(const T &  container){
+    std::for_each(std::begin(container), std::end(container), [](const auto &e)
+                  { std::cout << e << std::endl; });
+}
